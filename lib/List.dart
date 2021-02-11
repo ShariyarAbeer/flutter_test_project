93836cdc32a920fd07b0347ec6802f0cda1a75d2
ShariyarAbeer <007abeershariyar@gmail.com>
2021-02-12 04:20:40 +0600

    subtile add on list

--- lib/List.dart
+++ lib/List.dart
@@ -7,13 +7,23 @@ class Listtype extends StatefulWidget {
 
 class _ListtypeState extends State<Listtype> {
   List data = ['list1', 'list2', 'list3'];
+  List todo = [
+    {'number': '36', 'adress': '1/25', 'phon': '789'},
+    {'number': '37', 'adress': '2/25', 'phon': '412'},
+    {'number': '38', 'adress': '3/25', 'phon': '563'},
+    {'number': '39', 'adress': '4/25', 'phon': '526'},
+  ];
+  List adrs = ['1/36', '2/36', '56/7'];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return Container(
-          child: ListTile(title: Text(this.data[index])),
+          child: ListTile(
+            title: Text(this.data[index]),
+            subtitle: Text(this.adrs[index]),
+          ),
         );
       },
       itemCount: data.length,
